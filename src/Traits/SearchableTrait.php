5cbf3c72dde494a34730d279abe3845e204e5e2c
Venelin Iliev <venelin@provision.bg>
2020-10-26 11:22:11 +0200

    array index fix

--- src/Traits/SearchableTrait.php
+++ src/Traits/SearchableTrait.php
@@ -22,233 +22,254 @@ use ProVision\Searchable\Observers\ModelObserver;
 trait SearchableTrait
 {
 
-    /**
-     * Searchable score for results orders
-     * @var string
-     */
-    protected $searchableScoreKey = 'searchableScore';
+	/**
+	 * Searchable score for results orders
+	 *
+	 * @var string
+	 */
+	protected $searchableScoreKey = 'searchableScore';
 
-    /**
-     * Boot the trait.
-     * @return void
-     */
-    public static function bootSearchableTrait(): void
-    {
-        static::observe(new ModelObserver());
-    }
+	/**
+	 * Boot the trait.
+	 *
+	 * @return void
+	 */
+	public static function bootSearchableTrait(): void
+	{
+		static::observe(new ModelObserver());
+	}
 
-    /**
-     * @return MorphTo
-     */
-    public function searchable(): MorphTo
-    {
-        return $this->morphTo();
-    }
+	/**
+	 * @return MorphTo
+	 */
+	public function searchable(): MorphTo
+	{
+		return $this->morphTo();
+	}
 
-    /**
-     * Scope a query to searchable index
-     *
-     * @param Builder $query
-     * @param string $keywords
-     * @param SearchableModes|string $searchMode
-     * @return Builder
-     */
-    public function scopeSearch($query, string $keywords, string $searchMode = null): Builder
-    {
+	/**
+	 * Scope a query to searchable index
+	 *
+	 * @param Builder                $query
+	 * @param string                 $keywords
+	 * @param SearchableModes|string $searchMode
+	 *
+	 * @return Builder
+	 */
+	public function scopeSearch($query, string $keywords, string $searchMode = null): Builder
+	{
 
-        /*
-         * Set weight of fields
-         */
-        $titleWeight = str_replace(',', '.', (float)config('searchable.weight.title', 1.5));
-        $contentWeight = str_replace(',', '.', (float)config('searchable.weight.content', 1.0));
+		/*
+		 * Set weight of fields
+		 */
+		$titleWeight = str_replace(',', '.', (float) config('searchable.weight.title', 1.5));
+		$contentWeight = str_replace(',', '.', (float) config('searchable.weight.content', 1.0));
 
-        /*
-         * Clean keywords
-         */
-        if (!empty(config('searchable.cleaners.' . $searchMode)) && is_array(config('searchable.cleaners.' . $searchMode))) {
+		/*
+		 * Clean keywords
+		 */
+		if (!empty(config('searchable.cleaners.'.$searchMode)) && is_array(config('searchable.cleaners.'.$searchMode))) {
 
-            /** @var array $cleaners */
-            $cleaners = config('searchable.cleaners.' . $searchMode);
+			/** @var array $cleaners */
+			$cleaners = config('searchable.cleaners.'.$searchMode);
 
-            /** @var string $cleanerClass */
-            foreach ($cleaners as $cleanerClass) {
-                $keywords = (new $cleanerClass($keywords, $searchMode))->clean();
-            }
+			/** @var string $cleanerClass */
+			foreach ($cleaners as $cleanerClass) {
+				$keywords = (new $cleanerClass($keywords, $searchMode))->clean();
+			}
 
-        }
+		}
 
-        return $query->selectRaw(
-            $this->getTable() . '.*, ' .
-            '(
-                ' . $titleWeight . ' * (MATCH (' . config('searchable.table_name') . '.title) AGAINST (? ' . $searchMode . ')) +
-                ' . $contentWeight . ' * (MATCH (' . config('searchable.table_name') . '.title, ' . config('searchable.table_name') . '.content) AGAINST (? ' . $searchMode . '))
-                ) as ' . $this->searchableScoreKey
-            , [$keywords, $keywords])
-            ->leftJoin(config('searchable.table_name'), function (JoinClause $join) {
-                $join->on(config('searchable.table_name') . '.searchable_id', '=', $this->getTable() . '.id')
-                    ->where(config('searchable.table_name') . '.searchable_type', '=', $this->getMorphClass());
-            })
-            ->whereRaw('MATCH(' . config('searchable.table_name') . '.title, ' . config('searchable.table_name') . '.content) AGAINST(? ' . $searchMode
-                . ')', [$keywords])
-            ->groupBy($this->getTable() . '.id')
-            ->searchableOrder();
-    }
+		return $query->selectRaw(
+			$this->getTable().'.*, '.
+			'(
+                '.$titleWeight.' * (MATCH ('.config('searchable.table_name').'.title) AGAINST (? '.$searchMode.')) +
+                '.$contentWeight.' * (MATCH ('.config('searchable.table_name').'.title, '.config('searchable.table_name').'.content) AGAINST (? '.$searchMode.'))
+                ) as '.$this->searchableScoreKey
+			, [$keywords, $keywords])
+			->leftJoin(config('searchable.table_name'), function (JoinClause $join) {
+				$join->on(config('searchable.table_name').'.searchable_id', '=', $this->getTable().'.id')
+					->where(config('searchable.table_name').'.searchable_type', '=', $this->getMorphClass());
+			})
+			->whereRaw('MATCH('.config('searchable.table_name').'.title, '.config('searchable.table_name').'.content) AGAINST(? '.$searchMode
+				.')', [$keywords])
+			->groupBy($this->getTable().'.id')
+			->searchableOrder();
+	}
 
-    /**
-     * Set searchableScore order direction
-     *
-     * @param Builder $query
-     * @param string $direction ASC | DESC
-     * @return Builder
-     */
-    public function scopeSearchableOrder($query, $direction = 'desc')
-    {
-        return $query->orderBy($this->searchableScoreKey, $direction);
-    }
+	/**
+	 * Set searchableScore order direction
+	 *
+	 * @param Builder $query
+	 * @param string  $direction ASC | DESC
+	 *
+	 * @return Builder
+	 */
+	public function scopeSearchableOrder($query, $direction = 'desc')
+	{
+		return $query->orderBy($this->searchableScoreKey, $direction);
+	}
 
-    /**
-     * @return string|null
-     */
-    public function getIndexContent()
-    {
-        return $this->getIndexDataFromColumns($this->getSearchableContentColumns());
-    }
+	/**
+	 * @return string|null
+	 */
+	public function getIndexContent()
+	{
+		return $this->getIndexDataFromColumns($this->getSearchableContentColumns());
+	}
 
-    /**
-     * @param array $columns
-     * @return string|null
-     */
-    protected function getIndexDataFromColumns(array $columns)
-    {
-        $indexData = [];
-        foreach ($columns as $column) {
-            if ($this->indexDataIsRelation($column)) {
-                $indexData[] = $this->searchableGetIndexValueFromRelation($column);
-            } else {
-	            $columnValue = $this->{$column};
-	            if(is_array($columnValue)){
-		            $columnValue = implode(' ', $columnValue);
-	            }
-	            $indexData[] = trim($columnValue);
-            }
-        }
+	/**
+	 * @param array $columns
+	 *
+	 * @return string|null
+	 */
+	protected function getIndexDataFromColumns(array $columns)
+	{
+		$indexData = [];
+		foreach ($columns as $column) {
+			if ($this->indexDataIsRelation($column)) {
+				$indexData[] = $this->searchableGetIndexValueFromRelation($column);
+			} else {
+				$columnValue = $this->{$column};
+				$indexData[] = $this->convertDataToString($columnValue);
+			}
+		}
 
-        /*
-         * Replace all non word characters with spaces
-         * Only words are indexed by full text search engine and can be searched.
-         * Non word characters isn't indexed, so it does not make sense to leave them in the search string.
-         */
-        return preg_replace('/[^\p{L}\p{N}_]+/u', ' ', implode(' ', array_filter($indexData)));
-    }
+		/*
+		 * Replace all non word characters with spaces
+		 * Only words are indexed by full text search engine and can be searched.
+		 * Non word characters isn't indexed, so it does not make sense to leave them in the search string.
+		 */
+		return preg_replace('/[^\p{L}\p{N}_]+/u', ' ', implode(' ', array_filter($indexData)));
+	}
 
-    /**
-     * @param $column
-     *
-     * @return bool
-     */
-    protected function indexDataIsRelation($column): bool
-    {
-        return (int)strpos($column, '.') > 0;
-    }
+	/**
+	 * @param $column
+	 *
+	 * @return bool
+	 */
+	protected function indexDataIsRelation($column): bool
+	{
+		return (int) strpos($column, '.') > 0;
+	}
 
-    /**
-     * @param $column
-     *
-     * @return string|null
-     */
-    protected function searchableGetIndexValueFromRelation($column)
-    {
-        $relations = explode('.', $column);
-        $columnSelect = array_pop($relations);
+	/**
+	 * @param $column
+	 *
+	 * @return string|null
+	 */
+	protected function searchableGetIndexValueFromRelation($column)
+	{
+		$relations = explode('.', $column);
+		$columnSelect = array_pop($relations);
 
-        return $this->searchableExtractDataFromRelation($this, $relations, $columnSelect);
-    }
+		return $this->searchableExtractDataFromRelation($this, $relations, $columnSelect);
+	}
 
-    /**
-     * Extract data on nested relation with unlimited level
-     * @param Model $currentRelation
-     * @param array $relations
-     * @param string $column
-     * @return string|null
-     */
-    protected function searchableExtractDataFromRelation(Model $currentRelation, array $relations, string $column)
-    {
-        $relationship = $currentRelation;
-        $remainingRelations = $relations;
+	/**
+	 * Extract data on nested relation with unlimited level
+	 *
+	 * @param Model  $currentRelation
+	 * @param array  $relations
+	 * @param string $column
+	 *
+	 * @return string|null
+	 */
+	protected function searchableExtractDataFromRelation(
+		Model $currentRelation,
+		array $relations,
+		string $column
+	): ?string {
+		$relationship = $currentRelation;
+		$remainingRelations = $relations;
 
-        foreach ($relations as $relation) {
+		foreach ($relations as $relation) {
 
-            array_shift($remainingRelations);
+			array_shift($remainingRelations);
 
-            $relationship = $relationship->{$relation};
+			$relationship = $relationship->{$relation};
 
-            if (is_countable($relationship)) {
-                $values = collect();
-                foreach ($relationship as $subRelation) {
-                    $values->push($this->searchableExtractDataFromRelation($subRelation, $remainingRelations, $column));
-                }
-                return $values->implode(', ');
-            }
-        }
+			if (is_countable($relationship)) {
+				$values = collect();
+				foreach ($relationship as $subRelation) {
+					$values->push($this->searchableExtractDataFromRelation($subRelation, $remainingRelations, $column));
+				}
+				return $values->implode(', ');
+			}
+		}
 
-        if (!is_countable($relationship)) {
-            try {
-                $relationValue = $relationship->{$column};
-            } catch (Exception $exception) {
-                $relationValue = '';
-            }
-            return $relationValue;
-        }
+		if (!is_countable($relationship)) {
+			try {
+				$relationValue = $relationship->{$column};
+			} catch (Exception $exception) {
+				$relationValue = '';
+			}
+			return $this->convertDataToString($relationValue);
+		}
 
-        return $relationship->pluck($column)->implode(', ');
+		return $relationship->pluck($column)->implode(', ');
 
-    }
+	}
 
-    /**
-     * @return array
-     */
-    abstract protected function getSearchableContentColumns(): array;
+	/**
+	 * @param string|array|null $data
+	 *
+	 * @return string|null
+	 */
+	protected function convertDataToString($data): ?string
+	{
+		if (is_array($data)) {
+			return trim(implode(' ', $data));
+		}
+		return trim($data);
+	}
 
-    public function getIndexTitle(): string
-    {
-        return $this->getIndexDataFromColumns($this->getSearchableTitleColumns());
-    }
+	/**
+	 * @return array
+	 */
+	abstract protected function getSearchableContentColumns(): array;
 
-    /**
-     * @return array
-     */
-    abstract protected function getSearchableTitleColumns(): array;
+	public function getIndexTitle(): string
+	{
+		return $this->getIndexDataFromColumns($this->getSearchableTitleColumns());
+	}
 
-    /**
-     * @return MorphOne
-     */
-    public function searchableIndex(): MorphOne
-    {
-        return $this->morphOne(SearchableIndex::class, 'searchable');
-    }
+	/**
+	 * @return array
+	 */
+	abstract protected function getSearchableTitleColumns(): array;
 
-    /**
-     * Update/Insert index record of model
-     * @return void
-     */
-    public function indexRecord(): void
-    {
-        if (!$this->searchableIndex) {
-            $this->searchableIndex = new SearchableIndex();
-            $this->searchableIndex->searchable()->associate($this);
-        }
+	/**
+	 * @return MorphOne
+	 */
+	public function searchableIndex(): MorphOne
+	{
+		return $this->morphOne(SearchableIndex::class, 'searchable');
+	}
 
-        $this->searchableIndex->updateIndex();
-    }
+	/**
+	 * Update/Insert index record of model
+	 *
+	 * @return void
+	 */
+	public function indexRecord(): void
+	{
+		if (!$this->searchableIndex) {
+			$this->searchableIndex = new SearchableIndex();
+			$this->searchableIndex->searchable()->associate($this);
+		}
 
-    /**
-     *
-     * @throws Exception
-     */
-    public function unIndexRecord(): void
-    {
-        if ($this->searchableIndex) {
-            $this->searchableIndex->delete();
-        }
-    }
+		$this->searchableIndex->updateIndex();
+	}
+
+	/**
+	 *
+	 * @throws Exception
+	 */
+	public function unIndexRecord(): void
+	{
+		if ($this->searchableIndex) {
+			$this->searchableIndex->delete();
+		}
+	}
 }
